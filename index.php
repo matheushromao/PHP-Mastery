2ce2c6fa695f8653a5901a800425a5ccf6dbcbd9
Matheus Romão <matheus_henrique_9@hotmail.com>
2026-08-24 02:01:53 -0300

    Ajustando o Título

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/estilo.css">
-    <title>Ensino de PHP</title>
+    <title>PHP Mastery</title>
 </head>
 <body>
     <header class="cabecalho">
