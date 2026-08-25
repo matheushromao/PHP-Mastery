33c80d642b9b6505d77214ba94a7cbed5a28ff0d
Matheus Romão <matheus_henrique_9@hotmail.com>
2026-08-25 01:53:24 -0300

    feat: Adicionando novos arquivos do modúlo 1 sendo eles: html.php, css.php, comentarios.php e desafio.php

--- index.php
+++ index.php
@@ -32,6 +32,21 @@
                                 Integração HTML
                             </a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=css">
+                                Integração CSS
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=comentarios">
+                                Comentários PHP
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=desafio">
+                                Desafio
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </nav>
